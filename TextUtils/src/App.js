48068884904c09f0c2d55fa6749435c5095c50e6
KenaDholakiya <kenadholakiyavbi@gmail.com>
2026-04-27 18:05:55 +0530

    te

--- TextUtils/src/App.js
+++ TextUtils/src/App.js
@@ -1,23 +1,13 @@
 import "./App.css";
+import Navbar from "./components/Navbar";
 
-let name="Kena";
 function App() {
   return (
     <>
-      <nav>
-        <li>Home</li>
-        <li>About</li>
-        <li>Contact</li>
-        
-      </nav>
-      <div className="container">
-        <h1>Hello! {name}</h1>
-        <p>Lorem, ipsum dolor sit amet consectetur adipisicing 
-          elit. Dolor magnam repellendus expedita eos dolores 
-          eveniet neque quaerat? Eligendi, laborum ea qui iste 
-          temporibus est voluptas quidem et hic quaerat omnis 
-          voluptates porro dicta officia.</p>
-      </div>
+    <Navbar title="TextUtils" aboutText="About"/>
+    {/*<Navbar title="TextUtils" />*/}
+
+     {/*<Navbar/>*/}
     </>
   );
 }
